68a8de654fb8168d090c55a7af1f972978bab78b
Nikolay <git@kolyaka.com>
2025-09-04 09:40:26 +0500

    change position

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         <Route path="/specializations" element={<Specializations />} />
       </Routes>
 
-      <Toaster />
+      <Toaster position={"top-center"} />
     </ApiContext.Provider>
   );
 }
